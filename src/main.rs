b104c5b2fce3bafe06db2078a759f9a5f5a12952
monokemonoke <kawabataryuhei@gmail.com>
2023-03-29 22:53:31 +0900

    add library inquire

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use inquire::{error::InquireResult, Text};
+
+fn main() -> InquireResult<()> {
+    let _message = Text::new("").with_help_message("").prompt()?;
+
+    println!("Your input: {}", _message);
+
+    Ok(())
 }
