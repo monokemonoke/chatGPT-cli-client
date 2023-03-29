8bba93ce623954f5598e6202142334b37f1cda3d
monokemonoke <kawabataryuhei@gmail.com>
2023-03-29 23:05:29 +0900

    write sample code to do http request

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use inquire::{error::InquireResult, Text};
+use inquire::Text;
 
-fn main() -> InquireResult<()> {
-    let _message = Text::new("").with_help_message("").prompt()?;
+#[tokio::main]
+async fn main() -> reqwest::Result<()> {
+    let _message = Text::new("").with_help_message("").prompt().unwrap();
 
     println!("Your input: {}", _message);
+
+    let body = reqwest::get("https://example.com").await?.text().await?;
+
+    println!("Body: {:?}", body);
 
     Ok(())
 }
