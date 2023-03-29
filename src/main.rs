da808def3d2c12ca5c1891d4d35d82669432ec88
monokemonoke <kawabataryuhei@gmail.com>
2023-03-29 23:39:36 +0900

    serdeを使う

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,47 @@
 // use inquire::Text;
 use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
+use serde::{Deserialize, Serialize};
 use std::env;
+
+#[derive(Serialize, Deserialize)]
+struct ChatRequest {
+    model: String,
+    prompt: String,
+    max_tokens: i32,
+    temperature: i32,
+}
+
+// {
+//     "id": "cmpl-uqkvlQyYK7bGYrRHQ0eXlWi7",
+//     "object": "text_completion",
+//     "created": 1589478378,
+//     "model": "text-davinci-003",
+//     "choices": [
+//       {
+//         "text": "\n\nThis is indeed a test",
+//         "index": 0,
+//         "logprobs": null,
+//         "finish_reason": "length"
+//       }
+//     ],
+//     "usage": {
+//       "prompt_tokens": 5,
+//       "completion_tokens": 7,
+//       "total_tokens": 12
+//     }
+//   }
 
 #[tokio::main]
 async fn main() -> reqwest::Result<()> {
     // let _message = Text::new("").with_help_message("").prompt().unwrap();
 
-    let json_request = format!(
-        r#"{{
-            "model": "text-davinci-003",
-            "prompt": "Say this is a test",
-            "max_tokens": 7,
-            "temperature": 0
-        }}"#
-    );
+    let request_json = ChatRequest {
+        model: String::from("text-davinci-003"),
+        prompt: String::from("Say this is a test"),
+        max_tokens: 7,
+        temperature: 0,
+    };
+    let request_json = serde_json::to_string(&request_json).unwrap();
 
     // println!("Your input: {}", _message);
 
@@ -25,12 +53,15 @@ async fn main() -> reqwest::Result<()> {
             AUTHORIZATION,
             format!("Bearer {}", env::var("OPENAI_API_KEY").unwrap()),
         )
-        .body(json_request)
+        .body(request_json)
         .send()
+        .await
+        .unwrap()
+        .text()
         .await
         .unwrap();
 
-    println!("{}", res.text().await.unwrap());
+    println!("{}", res);
 
     Ok(())
 }
