6a3e979bdb21dfd4ae22d43ccb889d1ac4e807bd
monokemonoke <kawabataryuhei@gmail.com>
2023-03-29 23:29:43 +0900

    とりあえず通信できるようになった

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
-use inquire::Text;
+// use inquire::Text;
+use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
+use std::env;
 
 #[tokio::main]
 async fn main() -> reqwest::Result<()> {
-    let _message = Text::new("").with_help_message("").prompt().unwrap();
+    // let _message = Text::new("").with_help_message("").prompt().unwrap();
 
-    println!("Your input: {}", _message);
+    let json_request = format!(
+        r#"{{
+            "model": "text-davinci-003",
+            "prompt": "Say this is a test",
+            "max_tokens": 7,
+            "temperature": 0
+        }}"#
+    );
 
-    let body = reqwest::get("https://example.com").await?.text().await?;
+    // println!("Your input: {}", _message);
 
-    println!("Body: {:?}", body);
+    let client = reqwest::Client::new();
+    let res = client
+        .post("https://api.openai.com/v1/completions")
+        .header(CONTENT_TYPE, "application/json")
+        .header(
+            AUTHORIZATION,
+            format!("Bearer {}", env::var("OPENAI_API_KEY").unwrap()),
+        )
+        .body(json_request)
+        .send()
+        .await
+        .unwrap();
+
+    println!("{}", res.text().await.unwrap());
 
     Ok(())
 }
